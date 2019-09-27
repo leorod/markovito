807b2614e4efdbdfe659f3a2673dab589d7d028d
Leonardo Rodriguez <lrodriguez@elementum.com>
2019-09-27 20:28:59 -0300

    Logging in as bot

--- markovito/data/telegram_scraper.py
+++ markovito/data/telegram_scraper.py
@@ -12,7 +12,6 @@ class TelegramScraper:
         async with TelegramClient(self.config['session_name'], self.config['api_id'], self.config['api_hash'], loop=asyncio.get_event_loop()) as client:
             await client.connect()
             if not await client.is_user_authorized():
-                client.send_code_request(self.config['phone'])
-                client.sign_in(self.config["phone"], input('Enter the code: '))
+                client.sign_in(bot_token=api_config['bot_token'])
             response = await client.get_entity(username)
             return response.id
